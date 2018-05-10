ccdcac17937d2c69127baf61e43b22e0b468a4d1
kuvasz-greenfox <mail.adam.kovacs@gmail.com>
2018-05-10 12:09:15 +0200

    another batch of exercises

---
+++ week-01/day-04/ts-workshop/cuboid.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+
+let ca: number = 5.;
+let cb: number = 3.;
+let cc: number = 6.;
+
+console.log(`Surface Area: ${2 * (ca + cb + cc)}`);
+console.log(`Volume: ${ca * cb * cc}`);
---
+++ week-01/day-04/ts-workshop/define-basic-info.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+
+let myName: string = 'Kuvasz';
+let myAge: number = 23;
+let myHeight: number = 1.83;
+let myMaritalStatus: boolean = false;
+
+
+console.log(myName);
+console.log(myAge);
+console.log(myHeight);
+console.log(myMaritalStatus);
