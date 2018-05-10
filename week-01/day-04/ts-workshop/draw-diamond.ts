adbdf8c95017ce2d64c58c2dcbdc0ad97a3f9036
kuvasz-greenfox <mail.adam.kovacs@gmail.com>
2018-05-10 14:39:36 +0200

    drawing awesome diamonds

---
+++ week-01/day-04/ts-workshop/draw-diamond.ts
@@ -0,0 +1,37 @@
+'use strict';
+export{}
+
+let lineCount: number = 7;
+
+// Write a program that draws a
+// diamond like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+//
+// The diamond should have as many lines as lineCount is
+
+let star, spaces: number;
+let diamond: string;
+
+for (let i : number = 0; i <= lineCount - 1; ++i) {
+  diamond = '';
+  spaces = Math.abs(Math.floor(lineCount / 2) - i);
+  star = lineCount - spaces * 2;
+
+  for (let j: number = 1; j <= spaces; j++) {
+    diamond += ' ';
+  }
+
+  for (let j: number = 1; j <= star; j++) {
+    diamond += '*';
+  }
+
+  console.log(diamond);
+  
+}
