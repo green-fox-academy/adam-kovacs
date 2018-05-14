5224173d8a64d035a673532fe7cd7c7e2d408e91
kuvasz-greenfox <mail.adam.kovacs@gmail.com>
2018-05-14 11:41:27 +0200

    updating exercise

--- week-02/day-01/sum.ts
+++ week-02/day-01/sum.ts
@@ -1,17 +1,13 @@
 // -  Write a function called `sum` that sum all the numbers until the given parameter
 // -  The function should return the result
 'use strict'
-export{}
-
-
+export { }
 
 function sum(limit: number) {
   let subSum: number = 0;
   for (let i: number = 1; i <= limit; i++) {
     subSum = subSum + i;
     console.log(i);
-    console.log(subSum);
-    
   }
   return subSum;
 }
