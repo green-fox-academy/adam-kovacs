b9d1423b93c4441d466d8e13fb52281084de9e49
kuvasz-greenfox <mail.adam.kovacs@gmail.com>
2018-05-14 13:22:34 +0200

    updating exercise

---
+++ week-02/day-01/append-a.ts
@@ -0,0 +1,8 @@
+// -  Create an array variable named `animals`
+//    with the following content: `["kuty", "macsk", "cic"]`
+// -  Add all elements an `"a"` at the end
+
+let animals = ['kuty', 'macsk', "cic"];
+
+animals = animals.map(x => x + 'a');
+console.log(animals);
