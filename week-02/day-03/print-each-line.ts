fdbea8dec085d2cfbeb72390f7cc54824d332e7f
kuvasz-greenfox <mail.adam.kovacs@gmail.com>
2018-05-16 11:30:29 +0200

    reformatting exercise

--- week-02/day-03/print-each-line.ts
+++ week-02/day-03/print-each-line.ts
@@ -2,19 +2,22 @@
 // each of lines form the file.
 // If the program is unable to read the file (for example it does not exists),
 // then it should print an error message like: 'Unable to read file: my-file.txt'
+'use strict'
+export { }
+
 declare function require(path: string): any;
 let fs = require('fs');
 let charEncoding = 'utf-8'
 
-function readFile (filePath) {
+function readFile(filePath) {
   try {
-    let out:any = fs.readFileSync(filePath, charEncoding);
+    let out: any = fs.readFileSync(filePath, charEncoding);
     out = out.split('\n'),
-    out.forEach(element => {
-      console.log(element);      
-    });
+      out.forEach(element => {
+        console.log(element);
+      });
     return null;
-  } catch(error) {
+  } catch (error) {
     console.log(`Unable to read file: ${filePath}`);
     return null;
   }
