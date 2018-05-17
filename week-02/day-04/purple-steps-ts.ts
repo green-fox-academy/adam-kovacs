899c5669674a6e8e8b92185d63a278ffeca97926
kuvasz-greenfox <mail.adam.kovacs@gmail.com>
2018-05-17 11:20:37 +0200

    updating exercise

---
+++ week-02/day-04/purple-steps-ts.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Reproduce this:
+// [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+
+ctx.fillStyle = 'rgb(176, 78, 240';
+for (let i: number = 0; i < 20; i++) {
+  ctx.moveTo(i * 10, i * 10);
+  ctx.strokeRect(i * 10, i * 10, 10, 10);
+  ctx.fillRect(i * 10, i * 10, 10, 10);
+}
