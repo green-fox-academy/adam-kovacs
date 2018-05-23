38241daf97f4efcf5c4c1554ae03110d1e87e005
kuvasz-greenfox <mail.adam.kovacs@gmail.com>
2018-05-23 10:06:59 +0200

    updating students.ts

--- week-03/day-03/students.ts
+++ week-03/day-03/students.ts
@@ -1,11 +1,24 @@
 'use strict'
 
 class Student {
-  name:string;
-  grades : number[];
+  name: string;
+  grades: number[];
+
+  constructor(name: string) {
+    this.name = name;
+    if (name === 'God') {
+      this.grades = [10, 10, 10];
+    } else {
+      this.grades = [];
+    }
+  }
 }
 
-let jozsi = new Student();
-jozsi.name = 'Jozsi';
+let jozsi = new Student('Joskagyerek');
+let god = new Student('God');
+//jozsi.name = 'Jozsi';
+//jozsi.grades = [1,2,3];
+jozsi.grades.push(5);
 
-console.log(jozsi);
+console.log(jozsi.name, jozsi.grades);
+console.log(god.name, god.grades);
