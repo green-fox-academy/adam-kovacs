d14f6c5c717bb7c46bcc0da0c674eb542caa56b3
kuvasz-greenfox <mail.adam.kovacs@gmail.com>
2018-05-24 14:26:17 +0200

    cleaning up yellow-squares.ts

--- week-03/day-04/yellow-squares.ts
+++ week-03/day-04/yellow-squares.ts
@@ -4,16 +4,14 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 let maxDepth: number = parseInt(window.prompt('Enter fractal depth', '6'));
 let depth: number = 0;
-let delay;
 
-function drawSquare(x, y, width, height) {
+function drawSquare(x: number, y: number, width: number, height: number) {
   depth++;
   if (depth >= maxDepth) {
-    console.log(`return depth ${depth}`);
     depth--;
-    return
-    
+    return   
   }
+
   ctx.strokeRect(x, y + height / 3, width, height / 3);
   ctx.strokeRect(x + width / 3, y, width / 3, height);
 
@@ -21,7 +19,7 @@ function drawSquare(x, y, width, height) {
   drawSquare(x + width / 3, y, width / 3, height / 3);
   drawSquare(x + width * 2 / 3, y + height / 3, width / 3, height / 3);
   drawSquare(x + width / 3, y + height * 2 / 3, width / 3, height / 3);
-  console.log(`return from level ${depth}`);
+  
   depth--;
   return;
 }
