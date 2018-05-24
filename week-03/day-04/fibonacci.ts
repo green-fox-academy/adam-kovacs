4a564f10a490fef2f8a6b7e95b6baeb9783afa59
kuvasz-greenfox <mail.adam.kovacs@gmail.com>
2018-05-24 14:11:15 +0200

    restyleing fibonacci.ts

--- week-03/day-04/fibonacci.ts
+++ week-03/day-04/fibonacci.ts
@@ -10,9 +10,9 @@ function fibonacci(n: number): number {
   if (n === 1) {
     return 0;
   } else if (n === 2) {
-    return 1 + fibonacci(n-1);
+    return 1 + fibonacci(n - 1);
   } else {
-    return fibonacci(n-1) + fibonacci(n -2);
+    return fibonacci(n - 1) + fibonacci(n - 2);
   }
 }
 
