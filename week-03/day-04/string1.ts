b1209285ce580bb76f9a74cdaf8ea46c96c94f58
kuvasz-greenfox <mail.adam.kovacs@gmail.com>
2018-05-24 10:28:33 +0200

    string1.ts solved

---
+++ week-03/day-04/string1.ts
@@ -0,0 +1,13 @@
+// Given a string, compute recursively (no loops) a new string where all the
+// lowercase 'x' chars have been changed to 'y' chars.
+'use strict'
+
+function string1(str: string): string {
+  if (str.indexOf('x') === -1) {
+    return str;
+  }
+  console.log(str);
+  return string1(str.replace('x', 'y'));
+}
+
+console.log(string1('xoxoxoxo'));
