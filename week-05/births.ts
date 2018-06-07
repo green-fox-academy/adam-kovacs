6709fd291513e50134f098734db60be3a6da9361
kuvasz-greenfox <mail.adam.kovacs@gmail.com>
2018-06-07 10:11:50 +0200

    updating births.ts

--- week-05/births.ts
+++ week-05/births.ts
@@ -7,14 +7,16 @@ let charEncode = 'utf-8';
 function countBirths(fileName: string) {
   let births = {};
   fs.readFileSync(fileName, charEncode).split('\r\n').forEach(element => {
-    element = element.split(';')[1].slice(0, 4);
+    element = element.split(';')[1].slice(0, 4);    
     if (births[element]) {
       births[element]++;
     } else {
       births[element] = 1;
     }
   });
-  return Object.keys(births).reduce((a, b) => births[a] > births[b] ? a : b);
+  console.log(births);
+  
+  return Object.keys(births).reduceRight((a, b) => births[a] > births[b] ? a : b);
 }
 
 console.log(countBirths('births.csv'));
