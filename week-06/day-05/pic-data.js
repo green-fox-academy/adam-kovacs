0df454fb01eb416c6628ba26172a3132c8abc907
kuvasz-greenfox <mail.adam.kovacs@gmail.com>
2018-06-22 09:51:39 +0200

    uploading inital state

---
+++ week-06/day-05/pic-data.js
@@ -0,0 +1,10 @@
+export let picdata = [
+  {title: "Welcome to the Cyberpunk", story: "lorem", src: "/static/red-chinese.jpg"},
+  {title: "Abstract", story: "lorem", src: "/static/abstract.png"},
+  {title: "Seattle Streets", story: "lorem", src: "/static/cp-street.jpg"},
+  {title: "Heoi Suburb", story: "lorem", src: "/static/heoi.jpg"},
+  {title: "Hotel Marina", story: "lorem", src: "/static/hotel-marina.jpg"},
+  {title: "Chinese Neon", story: "lorem", src: "/static/neon.jpg"},
+  {title: "New Shanghai", story: "lorem", src: "/static/new-shanghai.jpg"},
+  {title: "Red Sky", story: "lorem", src: "/static/red-sky.png"},
+]
