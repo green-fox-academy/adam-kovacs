ebb012065abf0473c2afe5fbdadd22554af1d1db
kuvasz-greenfox <mail.adam.kovacs@gmail.com>
2018-06-26 14:42:26 +0200

    starwars search engine added

--- week-07/day-02/starwars/main.js
+++ week-07/day-02/starwars/main.js
@@ -1,9 +1,53 @@
 'use strict'
 
-let httpRequest = new XMLHttpRequest;
+console.log('script started')
+const form = document.querySelector('form')
+console.log(form);
+const charaList = document.querySelector('#chara-list');
+let charaInput;
 
-httpRequest.onload(() => {
+let processInput = () => {
+  console.log('input processed')
+  charaInput = document.querySelector('#chara-input')
+    .value
+    .toLowerCase()
+    .split(' ')
+    .join('+');
+}
+
+let getCharaPage = (pageCount) => {
+  const charaRequest = new XMLHttpRequest;
+  console.log(`start getting page #${pageCount}`)
+
+  charaRequest.onload = () => {
+    if (charaRequest.status === 200 && charaRequest.readyState === 4) {
+      addListElements(JSON.parse(charaRequest.response).results)
+      if (JSON.parse(charaRequest.responseText).next) {
+        getCharaPage(pageCount + 1);
+      }
+    }
+  };
+  charaRequest.open('GET', `https://swapi.co/api/people/?page=${pageCount}&search=${charaInput}`);
+  charaRequest.send('null');
+}
+
+let addListElements = (charaResponseList) => {
+  charaResponseList.forEach(element => {
+    let newChild = document.createElement('li');
+    newChild.className = "character";
+    newChild.textContent = element.name;
+    charaList.appendChild(newChild);
+  });
+}
+
+form.addEventListener('submit', function (event) {
+  while (charaList.firstChild) {
+    charaList.removeChild(charaList.firstChild);
+  }
+  event.preventDefault();
+  console.log('submitted');
+  processInput()
+  console.log(`input: ${charaInput}`);
+  getCharaPage(1);
 
 });
-
-httpRequest.open('')
