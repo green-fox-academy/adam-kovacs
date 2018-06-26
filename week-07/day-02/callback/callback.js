057b46589a88fe244fb58c2872e50ee0f5d06a37
kuvasz-greenfox <mail.adam.kovacs@gmail.com>
2018-06-26 12:16:31 +0200

    callback solved

--- week-07/day-02/callback/callback.js
+++ week-07/day-02/callback/callback.js
@@ -3,27 +3,25 @@
 const mapWith = (array, callback) => {
   let output = [];
 
-  // The mapWith() function should iterate over the given array and call the callback function on every element.
-  // It stores the callback return values in the output.
-  // The mapWith() should return with the output array.
+  array.forEach(element => {
+    output.push(callback(element));
+  });
 
-  return output
+  return output;
+}
+
+const removeSecondLetter = (word) => {
+  let newWord = '';
+  for (let i = 0; i < word.length; i += 2) {
+      newWord += (word[i]);
+    }
+  return newWord;
 }
 
 const addOne = (number) => {
   return number + 1;
 }
 
-// Exercise 1:
-
-console.log(mapwith([1, 2, 3], addOne));
-//expected result: [2, 3, 4]
-
-// Exercise 2:
-
-// Create a callback function which remove every second letter from a string
-
 const words = ['map', 'reduce', 'filter'];
-
-console.log(mapwith(words, removeSecondLetter));
-// expected result: ['mp','rdc', 'fle']
+console.log(mapWith([1, 2, 3], addOne));
+console.log(mapWith(words, removeSecondLetter));
