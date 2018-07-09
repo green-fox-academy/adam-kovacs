62802447fcbec01b7926aeca19afb6fb8d298f51
kuvasz-greenfox <mail.adam.kovacs@gmail.com>
2018-07-09 13:05:22 +0200

    multiplier solved

---
+++ week-09/day-01/multiplier.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function multiplier(a) {
+  return function (b) {
+    return a * b;
+  }
+}
+
+const duplicator = multiplier(2);
+
+console.log(duplicator(5)); // should log 10
+console.log(duplicator(10)); // should log 20
+
+const threeTimes = multiplier(3);
+
+console.log(threeTimes(5)); // should log 15
+console.log(threeTimes(100)); // should log 300
